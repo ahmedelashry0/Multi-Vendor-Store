77627a91f7d436cf5ea38649bbb4a9db0cd0dbc8
ahmedelashry0 <elashryprogramming@gmail.com>
2024-09-01 08:17:09 +0300

    13.2 try

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Category;
-use App\Models\Product;
-use App\Models\Store;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,14 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 //         \App\Models\User::factory(2)->create();
-
+        \App\Models\Admin::factory(3)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 //        Store::factory(5)->create();
-        Category::factory(10)->create();
-        Product::factory(10)->create();
+//        Category::factory(10)->create();
+//        Product::factory(10)->create();
 //        $this->call(UserSeeder::class);
     }
 }
